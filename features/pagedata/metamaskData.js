bf5c621adf85d5624c62adb7f21825c9c182dddf
em <em@ems-MacBook-Pro.local>
2022-02-07 12:52:13 +0000

    chore: first commit
    bdd tests using metamask extension

---
+++ features/pagedata/metamaskData.js
@@ -0,0 +1,8 @@
+module.exports = {
+
+    metaMaskInformation : 
+        {
+            secretKey:"moon pole cupboard scout sniff blade fee chief arrow anger prosper finger",
+            password :"12345678"
+        }
+    }
